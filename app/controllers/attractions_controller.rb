bac8e28a7e41bb3cd4c9099182bf136f4e723c2a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-01 16:01:03 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -10,8 +10,8 @@ class AttractionsController < ApplicationController
 
   def ride
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    @ride.take_ride
-    flash[:notice] = "Thanks for riding the #{Attraction.find(@ride.attraction_id).name}!"
+    #@ride.take_ride
+    flash[:notice] = @ride.take_ride
     redirect_to user_path(@ride.user)
   end
 end
