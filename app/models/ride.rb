537eed82baf8d673a0ec207f99e2ea70a6270e57
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-01 14:42:25 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,17 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  # this has a user and a attraction object
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if user.height < attraction.min_height && user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif  user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+     "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.update(:tickets => "#{user.tickets - attraction.tickets}", :nausea => "#{user.nausea + attraction.nausea_rating}", :happiness => "#{user.happiness + attraction.happiness_rating}")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-
-
+  root "welcome#home"
+  #get "/users/new", to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  resources :users
+  resources :attractions
+  resources :sessions, only: [:create, :destroy]
 end
+
+
+# root GET    /                                 welcome#home
+# users_new GET       /users/new(.:format)      users#new
+# sessions POST       /sessions(.:format)       sessions#create
+# new_session GET     /sessions/new(.:format)   sessions#new
+# session DELETE      /sessions/:id(.:format)   sessions#destroy
