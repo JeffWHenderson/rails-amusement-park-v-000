f8f6491991a643d97a7db27d6c25ac9a3f59d318
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-01 14:53:30 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user
+      raise @user.id.inspect
       session[:user_id] = @user.id
       redirect_to user_path @user
     else
