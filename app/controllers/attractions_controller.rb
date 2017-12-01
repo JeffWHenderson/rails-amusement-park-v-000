4d8ae22cb4f1cf9ee6fcead2f5815dd1afef5e7d
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-01 16:20:27 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -14,6 +14,7 @@ class AttractionsController < ApplicationController
   end
 
   def create
+    raise attraction_params(params).inspect
     @attraction = Attraction.new(attraction_params(params))
     raise @attraction.inspect
   end
