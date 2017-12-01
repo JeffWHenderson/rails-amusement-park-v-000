9abe271aaf485f1bf89a173dd946bf5c8b14068a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-01 14:47:08 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   def show
     if session[:user_id]
-      raise User.find(8).inspect
+      raise User.find(params[:id]).inspect
       @user = User.find_by(params[:id])
       puts @user.admin
     else
